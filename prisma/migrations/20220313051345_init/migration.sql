73e5f99c64c8c54b5d2b98cf62b0b8b36d18535b
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-03-14 17:28:45 -0400

    feat(prisma): postgres on heroku

--- prisma/migrations/20220313051345_init/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Person" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL
-);
---
+++ prisma/migrations/20220314211808_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Peep" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+
+    CONSTRAINT "Peep_pkey" PRIMARY KEY ("id")
+);
