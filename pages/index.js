8a028789f94d3b8c3de08495fc5fd904d83d66a6
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-03-13 20:19:24 -0400

    feat(prisma): ORM

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function getServerSideProps() {
+  const data = await prisma.person.findMany();
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+function HomePage({ data }) {
+  const people = data.map((peep) => (
+    <li key={peep.id}>{`${peep.firstName} ${peep.lastName}`}</li>
+  ));
+
+  return (
+    <div>
+      <h1>List of peeps from a sql database</h1>
+      <ul>{people}</ul>
+    </div>
+  );
 }
 
 export default HomePage;
---
+++ prisma/migrations/20220313051345_init/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Person" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL
+);
